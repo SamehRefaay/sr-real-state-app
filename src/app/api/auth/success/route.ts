06517d5e9c10aca162eba2c3104407917cff8eb0
SamehRefaay <samehrefaay89@gmail.com>
2024-10-06 19:26:26 +0300

    fixing some issues

--- src/app/api/auth/success/route.ts
+++ src/app/api/auth/success/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import prisma from '@/lib/prisma';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { NextResponse } from 'next/server';
